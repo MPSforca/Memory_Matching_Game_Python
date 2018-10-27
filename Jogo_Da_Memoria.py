0d9b9e8b2a00907a00f79d303178487a00041e1c
Matheus Pierre Sforça <matheussforza@gmail.com>
2018-10-27 17:27:39 -0300

    Flips the card on click

--- Jogo_Da_Memoria.py
+++ Jogo_Da_Memoria.py
@@ -7,9 +7,15 @@ import random
 images_name = ["costas.jpg", "carta1.jpg", "carta2.jpg", "carta3.jpg",
                "carta4.jpg", "carta5.jpg", "carta6.jpg", "carta7.jpg",
                "carta8.jpg"]
+# List with every image loaded from the images_name
 images = []
+# Position of every card
+cards_positions = []
+# Board that the player is seeing
 shown_board = []
+# Board that represents every card flipped
 actual_board = []
+# Card data:
 card_width = 170
 card_height = 150
 
@@ -25,6 +31,8 @@ def start():
     # Load and create images from path
     for image in images_name:
         card = pygame.image.load(os.path.join("imagens", image))
+        # Set image size:
+        card = pygame.transform.scale(card, (card_width, card_height))
 
         # It's not the first image (Card's back)
         if len(images) > 0:
@@ -37,33 +45,46 @@ def start():
     # Shuffle the board
     random.shuffle(actual_board)
 
-    # The board that is being shown for the user (images[0] = card's back img)
     global shown_board
     shown_board = [images[0]] * 16
 
     # Updates the board that is being shown for the player
-    _update_shown_board()
+    create_board()
 
 
-def _update_shown_board():
+def create_board():
+    cards_back = images[0]
+
     # Fill the board with images
     for i in range(0, 16):
-        # Get the image that represents card's back:
-        current_card = shown_board[i]
-
-        # Set image size:
-        current_card = pygame.transform.scale(current_card, 
-                                              (card_width, card_height))
-
-        rect = current_card.get_rect()
 
         # Card position:
         x = 50 + card_width * (i % 4) + 40 * (i % 4)
         y = 20 + card_height * (i // 4) + 20 * (i // 4)
-        rect = rect.move(x, y)
 
-        # Print image on screen:
-        screen.blit(current_card, rect)
+        rect = pygame.Rect(x, y, card_width, card_height)
+
+        global cards_positions
+        cards_positions.append(rect)
+
+        screen.blit(cards_back, rect)
+
+
+def click_handler(mouse_x, mouse_y):
+    for i in range(0, 16):
+        if cards_positions[i].collidepoint(mouse_x, mouse_y):
+            if shown_board[i] == images[0]:  # The card is backwards:
+                shown_board[i] = actual_board[i]
+                update_board()
+            break
+
+
+def update_board():
+    for i in range(0, 16):
+        current_position = cards_positions[i]
+
+        # Print image on screen and save it in a list (incluiding its position)
+        screen.blit(shown_board[i], current_position)
 
 
 def run():
@@ -71,6 +92,9 @@ def run():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                clicked_x, clicked_y = pygame.mouse.get_pos()
+                click_handler(clicked_x, clicked_y)
 
         pygame.display.flip()
 
