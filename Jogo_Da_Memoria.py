decd5e5b3b49a2078259bc4039338aee1c1d1c56
Matheus Pierre Sforça <matheussforza@gmail.com>
2018-10-27 21:20:17 -0300

    Added menu with Score and Time counters

--- Jogo_Da_Memoria.py
+++ Jogo_Da_Memoria.py
@@ -16,7 +16,7 @@ shown_board = []
 # Board that represents every card flipped
 actual_board = []
 # Card data:
-card_width = 170
+card_width = 107
 card_height = 150
 # Game data:
 first_flipped_card_index = None
@@ -25,6 +25,8 @@ is_wrong = False
 last_wrong_time = 0
 how_many_pairs = 0
 score = 0
+match_start_time = None
+match_time = 0
 
 pygame.init()
 pygame.display.set_caption("Jogo da Memória")
@@ -55,8 +57,36 @@ def start():
     global shown_board
     shown_board = [images[0]] * 16
 
+    draw_menu()
+
     # Updates the board that is being shown for the player
     create_board()
+
+    global match_start_time
+    match_start_time = pygame.time.get_ticks()
+
+
+def draw_menu():
+    menu_rect = pygame.Rect(0, 0, 250, 700)
+    pygame.draw.rect(screen, (99, 0, 0), menu_rect)
+
+    # Logo:
+    logo = pygame.image.load(os.path.join("imagens", "logo.jpg"))
+    logo = pygame.transform.scale(logo, (250, 100))
+    rect = pygame.Rect(0, 0, 250, 100)
+    screen.blit(logo, rect)
+    write_game_data()
+
+
+# To write game pontuation and time
+def write_game_data():
+
+    f = pygame.font.Font(os.path.join("fonts", "Montserrat-Regular.ttf"), 20)
+    text = f.render("Pontuação: " + str(score), 1, (255, 255, 255))
+    screen.blit(text, (20, 150, 30, 210))
+
+    text = f.render("Tempo: " + str(match_time), 1, (255, 255, 255))
+    screen.blit(text, (20, 180, 30, 210))
 
 
 def create_board():
@@ -66,7 +96,7 @@ def create_board():
     for i in range(0, 16):
 
         # Card position:
-        x = 50 + card_width * (i % 4) + 40 * (i % 4)
+        x = 300 + card_width * (i % 4) + 40 * (i % 4)
         y = 20 + card_height * (i // 4) + 20 * (i // 4)
 
         rect = pygame.Rect(x, y, card_width, card_height)
@@ -93,13 +123,15 @@ def click_handler(mouse_x, mouse_y):
                     # Wrong pair:
                     if (actual_board[first_flipped_card_index] !=
                             actual_board[second_flipped_card_index]):
-                        global is_wrong, last_wrong_time
+                        global is_wrong, last_wrong_time, score
                         last_wrong_time = pygame.time.get_ticks()
                         is_wrong = True
+                        score -= 1
                     else:
                         first_flipped_card_index = None
                         second_flipped_card_index = None
                         is_wrong = False
+                        score += 3
                 else:
                     first_flipped_card_index = i
             break
@@ -136,6 +168,10 @@ def run():
         if is_wrong and pygame.time.get_ticks() - last_wrong_time >= 1000:
             wrong_pair()
 
+        global match_time
+        match_time = (pygame.time.get_ticks() - match_start_time) // 1000
+
+        draw_menu()
         pygame.display.flip()
 
 
