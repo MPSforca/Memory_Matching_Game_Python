5e1dffcebb81c15c799b0c0f37a1c7dc756d6e7f
Matheus Pierre Sforça <matheussforza@gmail.com>
2018-11-06 00:00:11 -0200

    Code Refactoring
    - Added class Card and changed the way cards are assigned to board;
    - Changed the way to keep the current pair;

--- Jogo_Da_Memoria.py
+++ Jogo_Da_Memoria.py
@@ -4,14 +4,10 @@ import sys
 import random
 import ctypes
 
-# Element at 0: Card back
-IMAGES_NAME = ["costas", "carta1", "carta2", "carta3",
-               "carta4", "carta5", "carta6", "carta7",
-               "carta8"]
+IMAGES_NAME = ["carta1", "carta2", "carta3", "carta4", "carta5", "carta6", "carta7", "carta8"]
 
 IMAGES_EXTENSION = ".png"
 
-# Window Data
 WINDOW_WIDTH = 900
 WINDOW_HEIGHT = 700
 
@@ -19,28 +15,16 @@ WINDOW_HEIGHT = 700
 CARD_WIDTH = 105
 CARD_HEIGHT = 155
 
-# List with every image loaded from the images_name
-image_back = None
+MENU_WIDTH = 250
+MENU_HEIGHT = WINDOW_HEIGHT
 
-# Position of every card
-cards_positions = []
+card_backward_image = None
+board = []
 
-# Board that the player is seeing
-shown_board = []
-
-# Board that represents every card flipped
-actual_board = []
-
-# Game data:
-first_flipped_card_index = None
-second_flipped_card_index = None
+current_pair = []
 is_wrong = False
 last_wrong_time = 0
 how_many_pairs = 0
-
-# Menu
-MENU_WIDTH = 250
-MENU_HEIGHT = WINDOW_HEIGHT
 
 score = 0
 match_time = 0
@@ -50,33 +34,42 @@ leave_button = None
 restart_button = None
 logo = None
 
+class Card:
+    card_name = ""
+    card_image = ""
+    card_rectangle = None
+    is_backward = True
+
+    def __init__(self, card_name, card_image, is_backward = True):
+        self.card_name = card_name
+        self.card_image = card_image
+        self.is_backward = is_backward
+
+    def set_rectangle(self, rect):
+        self.card_rectangle = rect
+
+    def flip_card(self):
+        self.is_backward = not self.is_backward
+
+
 pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Jogo da Memória")
 pygame.display.set_icon(pygame.image.load(os.path.join("images", IMAGES_NAME[0] + IMAGES_EXTENSION)))
 
-# Screen size and color
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 screen.fill((255, 255, 255))
 
-# Load the logo:
 logo = pygame.image.load(os.path.join("images", "logo.png"))
 logo = pygame.transform.scale(logo, (250, 100))
 
-# Load the images:
-for image in IMAGES_NAME:
-    card = pygame.image.load(os.path.join("images", image + IMAGES_EXTENSION))
+card_backward_image = pygame.image.load(os.path.join("images", "costas.png"))
 
-    # Set image resolution:
-    card = pygame.transform.scale(card, (CARD_WIDTH, CARD_HEIGHT))
+for card_name in IMAGES_NAME:
+    card_image = pygame.image.load(os.path.join("images", card_name + IMAGES_EXTENSION))
 
-    # It's not the first image (Card's back)
-    if image_back is not None:
-        # Adds twice => Two cards of each on board
-        actual_board.append(card)
-        actual_board.append(card)
-    else:
-        image_back = card
+    board.append(Card(card_name, card_image))
+    board.append(Card(card_name, card_image))
 
 
 def draw_menu():
@@ -104,39 +97,25 @@ def draw_menu():
 
 
 def start_match():
-    # Shuffle the board
-    random.shuffle(actual_board)
+    random.shuffle(board)
 
-    global shown_board
-    shown_board = [image_back] * 16
+    for card in board:
+        card.is_backward = True
 
     draw_menu()
-
-    # Updates the board that is being shown for the player
     create_board()
 
-    # Reset variables:
-    global score
+    global score, match_start_time
     score = 0
-
-    global match_start_time
     match_start_time = pygame.time.get_ticks()
 
-    global first_flipped_card_index, second_flipped_card_index, is_wrong
-    first_flipped_card_index = None
-    second_flipped_card_index = None
-    is_wrong = False
-
-    global last_wrong_time, how_many_pairs, match_time, match_is_running
-    last_wrong_time = 0
+    global how_many_pairs, match_time, match_is_running
     how_many_pairs = 0
     match_time = 0
     match_is_running = True
 
 
-# To write game pontuation and time
 def write_game_data():
-
     f = pygame.font.Font(os.path.join("fonts", "Montserrat-Regular.ttf"), 20)
     text = f.render("Pontuação: " + str(score), 1, (0, 0, 0))
     screen.blit(text, (20, 150, 30, 210))
@@ -146,19 +125,16 @@ def write_game_data():
 
 
 def create_board():
-    # Fill the board with images
-    for i in range(0, 16):
-
-        # Card position:
-        x = 300 + CARD_WIDTH * (i % 4) + 40 * (i % 4)
-        y = 20 + CARD_HEIGHT * (i // 4) + 20 * (i // 4)
-
+    current_index = 0
+    for card in board:
+        x = 300 + CARD_WIDTH * (current_index % 4) + 40 * (current_index % 4)
+        y = 20 + CARD_HEIGHT * (current_index // 4) + 20 * (current_index // 4)
         rect = pygame.Rect(x, y, CARD_WIDTH, CARD_HEIGHT)
 
-        global cards_positions
-        cards_positions.append(rect)
-
-        screen.blit(image_back, rect)
+        card.set_rectangle(rect)
+        cards_back = pygame.transform.scale(card_backward_image, (CARD_WIDTH, CARD_HEIGHT))
+        screen.blit(cards_back, card.card_rectangle)
+        current_index += 1
 
 
 def click_handler(mouse_x, mouse_y):
@@ -167,36 +143,35 @@ def click_handler(mouse_x, mouse_y):
     elif restart_button.collidepoint(mouse_x, mouse_y):
         start_match()
     else:
-        for i in range(0, 16):
-            if cards_positions[i].collidepoint(mouse_x, mouse_y) and shown_board[i] == image_back:
+        for card in board:
+            if card.card_rectangle.collidepoint(mouse_x, mouse_y) and card.is_backward:
+                card.flip_card()
+
                 pygame.mixer.stop()
                 pygame.mixer.Sound(os.path.join("sounds", "flip.wav")).play()
-                shown_board[i] = actual_board[i]
-                # Draws the card:
-                screen.blit(shown_board[i], cards_positions[i])
+
+                card_img = card.card_image
+                card_img = pygame.transform.scale(card_img, (CARD_WIDTH, CARD_HEIGHT))
+                screen.blit(card_img, card.card_rectangle)
                 pygame.display.flip()
-                global first_flipped_card_index
-                # There's already a card facing front:
-                if first_flipped_card_index is not None:
-                    global second_flipped_card_index
-                    second_flipped_card_index = i
-                    # Wrong pair:
-                    if (actual_board[first_flipped_card_index] != actual_board[second_flipped_card_index]):
-                        global is_wrong, last_wrong_time, score
+
+                current_pair.append(card)
+                global is_wrong, score
+                if len(current_pair) == 2:
+                    if current_pair[0].card_name != current_pair[1].card_name:
+                        global last_wrong_time
                         last_wrong_time = pygame.time.get_ticks()
                         is_wrong = True
-                        score -= 1
-                    else:
+                        score -= 1  
+                    else: 
                         pygame.mixer.stop()
                         pygame.mixer.Sound(os.path.join("sounds", "point.wav")).play()
-                        first_flipped_card_index = None
-                        second_flipped_card_index = None
+                        global how_many_pairs
                         is_wrong = False
                         score += 3
-                        global how_many_pairs
                         how_many_pairs += 1
-                else:
-                    first_flipped_card_index = i
+                        current_pair.clear()
+                        
                 break
 
 
@@ -204,32 +179,30 @@ def wrong_pair():
     pygame.mixer.stop()
     pygame.mixer.Sound(os.path.join("sounds", "error.wav")).play()
 
-    global first_flipped_card_index, second_flipped_card_index
-    shown_board[first_flipped_card_index] = image_back
-    shown_board[second_flipped_card_index] = image_back
+    card1 = current_pair[0]
+    card2 = current_pair[1]
 
-    screen.blit(shown_board[first_flipped_card_index], cards_positions[first_flipped_card_index])
+    cards_back = pygame.transform.scale(card_backward_image, (CARD_WIDTH, CARD_HEIGHT))
 
-    screen.blit(shown_board[second_flipped_card_index], cards_positions[second_flipped_card_index])
+    screen.blit(cards_back, card1.card_rectangle)
+    screen.blit(cards_back, card2.card_rectangle)
 
-    first_flipped_card_index = None
-    second_flipped_card_index = None
+    current_pair[0].flip_card()
+    current_pair[1].flip_card()
 
+    current_pair.clear()
     global is_wrong, last_wrong_time
     is_wrong = False
     last_wrong_time = 0
 
 
 def check_win():
-    # Victory
     if how_many_pairs == 8:
         pygame.mixer.stop()
         pygame.mixer.Sound(os.path.join("sounds", "victory.wav")).play()
         global match_is_running
         match_is_running = False
-        ctypes.windll.user32.MessageBoxW(0, "Parabéns! Você venceu o " +
-                                            "jogo fazendo " + str(score) +
-                                            " pontos!", "Fim de Jogo!", 1)
+        ctypes.windll.user32.MessageBoxW(0, "Parabéns! Você venceu o jogo fazendo " + str(score) + " pontos!", "Fim de Jogo!", 1)
 
 
 def run():
@@ -246,7 +219,6 @@ def run():
 
         if match_is_running:
             global match_time
-            # Calculates the match duration:
             match_time = (pygame.time.get_ticks() - match_start_time) // 1000
             draw_menu()
 
