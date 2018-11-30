2b22a6cc08454c5eda2c8ca4cd4c7221c4b03910
Matheus Pierre Sforça <matheussforza@gmail.com>
2018-11-29 22:00:30 -0200

    Added comments

--- Jogo_Da_Memoria.py
+++ Jogo_Da_Memoria.py
@@ -14,240 +14,295 @@ EXTENSAO_IMAGENS = ".png"
 WIDTH_TELA = 900
 HEIGHT_TELA = 700
 
-# Card data
-CARD_WIDTH = 105
-CARD_HEIGHT = 155
+# Dimensões das cartas em pixels:
+WIDTH_CARTA = 105
+HEIGHT_CARTA = 155
 
-MENU_WIDTH = 250
-MENU_HEIGHT = HEIGHT_TELA
+# Dimensões do menu lateral
+WIDTH_MENU = 250
+HEIGHT_MENU = HEIGHT_TELA
 
-card_backward_image = None
-board = []
+imagem_costas_carta = None  # Variável que representará a imagem das costas das cartas
+tabuleiro = []              # Vetor que representa o tabuleiro atual
 
-current_pair = []
-is_wrong = False
-last_wrong_time = 0
-how_many_pairs = 0
+# Variáveis para tratar do par atual:
+par_atual = []
+esta_errado = False
+tempo_ultimo_erro = 0
 
-score = 0
-match_time = 0
-match_start_time = None
-match_is_running = True
-leave_button = None
-restart_button = None
+# Variáveis que representam dados sobre a partida
+qtos_pares = 0              # Quantos pares o jogador já encontrou
+pontuacao = 0               # Pontuacao do usuário
+tempo_partida = 0           # Tempo desde o início da partida
+tempo_inicio_partida = None # Tempo que representa quando iniciou a partida
+partida_acontecendo = True  # Booleana que indica se a partida está acontecendo
+
+# Variáveis que serão utilizadas durante o menu
+botao_sair = None
+botao_reiniciar = None
 logo = None
 
 
-# Sounds
-score_sound = None
-flip_sound = None
-victory_sound = None
-error_sound = None
+# Sons
+som_pontuacao = None
+som_virada = None
+som_vitoria = None
+som_erro = None
 
-class Card:
-    card_name = ""
-    card_image = ""
-    card_rectangle = None
-    is_backward = True
+# Classe Carta que representará cada carta do jogo
+class Carta:
+    nome_carta = ""         # Representa o nome da carta
+    imagem_carta = ""       # Representa a imagem da carta
+    retangulo_carta = None  # Representa o retangulo no qual a carta está posicionada
+    de_costas = True        # Indica se a carta está virada de costas
 
-    def __init__(self, card_name, card_image, is_backward = True):
-        self.card_name = card_name
-        self.card_image = card_image
-        self.is_backward = is_backward
+    def __init__(self, nome_carta, imagem_carta, de_costas = True):
+        self.nome_carta = nome_carta
+        self.imagem_carta = imagem_carta
+        self.de_costas = de_costas
 
-    def set_rectangle(self, rect):
-        self.card_rectangle = rect
+    # Método para mudar a localização da imagem
+    def set_retangulo(self, rect):
+        self.retangulo_carta = rect
 
-    def flip_card(self):
-        self.is_backward = not self.is_backward
+    # Método para virar a carta
+    def virar_carta(self):
+        self.de_costas = not self.de_costas
 
+# Inicia o jogo
 pygame.init()
 pygame.mixer.init()
-pygame.display.set_caption("Jogo da Memória")
+pygame.display.set_caption("Jogo da Memória")       # Para mudar o título do programa
 pygame.display.set_icon(pygame.image.load(os.path.join("images", NOME_IMAGENS[0] + EXTENSAO_IMAGENS)))
 
+# Preenche a tela de branco (para iniciá-la) 
 screen = pygame.display.set_mode((WIDTH_TELA, HEIGHT_TELA))
 screen.fill((255, 255, 255))
 
-score_sound = pygame.mixer.Sound(os.path.join("sounds", "point.wav"))
-victory_sound = pygame.mixer.Sound(os.path.join("sounds", "victory.wav"))
-error_sound = pygame.mixer.Sound(os.path.join("sounds", "error.wav"))
-flip_sound = pygame.mixer.Sound(os.path.join("sounds", "flip.wav"))
+# Carrega os sons do jogo
+som_pontuacao = pygame.mixer.Sound(os.path.join("sounds", "point.wav"))
+som_vitoria = pygame.mixer.Sound(os.path.join("sounds", "victory.wav"))
+som_erro = pygame.mixer.Sound(os.path.join("sounds", "error.wav"))
+som_virada = pygame.mixer.Sound(os.path.join("sounds", "flip.wav"))
 
-
+# Carrega o logo do jogo e ajusta a sua escala
 logo = pygame.image.load(os.path.join("images", "logo.png"))
 logo = pygame.transform.scale(logo, (250, 100))
 
-card_backward_image = pygame.image.load(os.path.join("images", "costas.png"))
+# Carrega a imagem da parte de trás das cartas
+imagem_costas_carta = pygame.image.load(os.path.join("images", "costas.png"))
 
-for card_name in NOME_IMAGENS:
-    card_image = pygame.image.load(os.path.join("images", card_name + EXTENSAO_IMAGENS))
+# Para cada carta informada no vetor de cartas...
+for nome_carta in NOME_IMAGENS:
+    imagem_carta = pygame.image.load(os.path.join("images", nome_carta + EXTENSAO_IMAGENS)) # Adiciona sua imagem no vetor
 
-    board.append(Card(card_name, card_image))
-    board.append(Card(card_name, card_image))
+    # Adiciona duas vezes a mesma carta no tabuleiro porque o jogo é composto de pares
+    tabuleiro.append(Carta(nome_carta, imagem_carta))
+    tabuleiro.append(Carta(nome_carta, imagem_carta))
 
+# Reseta a tela
 screen.fill((255, 255, 255))
 pygame.time.delay(100)
 
 
-def draw_menu():
-    # Reset:
-    menu_rect = pygame.Rect(0, 0, MENU_WIDTH, MENU_HEIGHT)
-    pygame.draw.rect(screen, (255, 255, 255), menu_rect)
+# Método para desenhar o menu
+def desenhar_menu():
+    # Resetar o menu (Pinta-o inteiro de branco):
+    ret_menu = pygame.Rect(0, 0, WIDTH_MENU, HEIGHT_MENU)
+    pygame.draw.rect(screen, (255, 255, 255), ret_menu)
 
-    # Logo:
+    # Carregar a Logo e exibí-la no menu:
     global logo
     logo = pygame.transform.scale(logo, (250, 100))
-    rect = pygame.Rect(0, 0, MENU_WIDTH, 100)
+    rect = pygame.Rect(0, 0, WIDTH_MENU, 100)
     screen.blit(logo, rect)
-    write_game_data()
+    escrever_informacoes_jogo()
 
-    # Line:
-    pygame.draw.line(screen, (0, 0, 0), (MENU_WIDTH + 10, 0), (MENU_WIDTH + 10, MENU_HEIGHT))
+    # Linha lateral do menu:
+    pygame.draw.line(screen, (0, 0, 0), (WIDTH_MENU + 10, 0), (WIDTH_MENU + 10, HEIGHT_MENU))
 
-    # Buttons to leave and restart:
+    # Botões para reiniciar a partida e sair do jogo:
     f = pygame.font.Font(os.path.join("fonts", "Montserrat-Regular.ttf"), 20)
-    button_text = f.render("Reiniciar", 1, (0, 0, 0))
-    global restart_button
-    restart_button = screen.blit(button_text, (20, 600))
+    texto_botao = f.render("Reiniciar", 1, (0, 0, 0))
+    global botao_reiniciar
+    botao_reiniciar = screen.blit(texto_botao, (20, 600))
 
-    button_text = f.render("Sair", 1, (0, 0, 0))
-    global leave_button
-    leave_button = screen.blit(button_text, (20, 630))
+    texto_botao = f.render("Sair", 1, (0, 0, 0))
+    global botao_sair
+    botao_sair = screen.blit(texto_botao, (20, 630))
+
+# Método que resetará as principais variáveis de uma partida
+def iniciar_partida():
+    random.shuffle(tabuleiro)       # Embaralha o tabuleiro
+
+    # Vira todas as cartas de costas:
+    for carta in tabuleiro:
+        carta.de_costas = True
+
+    # Desenha o menu e chama o método para criar o tabuleiro
+    desenhar_menu()
+    criar_tabuleiro()
+
+    # Reseta a pontuação da partida e ajusta o tempo de início da mesma
+    global pontuacao, tempo_inicio_partida
+    pontuacao = 0
+    tempo_inicio_partida = pygame.time.get_ticks()
+
+    # Reseta a quantidade de pares encontrados pelo usuário e dados sobre o tempo da partida
+    global qtos_pares, tempo_partida, partida_acontecendo
+    qtos_pares = 0
+    tempo_partida = 0
+    partida_acontecendo = True
 
 
-def start_match():
-    random.shuffle(board)
-
-    for card in board:
-        card.is_backward = True
-
-    draw_menu()
-    create_board()
-
-    global score, match_start_time
-    score = 0
-    match_start_time = pygame.time.get_ticks()
-
-    global how_many_pairs, match_time, match_is_running
-    how_many_pairs = 0
-    match_time = 0
-    match_is_running = True
-
-
-def write_game_data():
+# Método para escrever as informações da partida no menu
+def escrever_informacoes_jogo():
+    # Escreve a pontuação atual do jogador:
     f = pygame.font.Font(os.path.join("fonts", "Montserrat-Regular.ttf"), 20)
-    text = f.render("Pontuação: " + str(score), 1, (0, 0, 0))
+    text = f.render("Pontuação: " + str(pontuacao), 1, (0, 0, 0))
     screen.blit(text, (20, 150, 30, 210))
 
-    text = f.render("Tempo: " + str(match_time) + "s", 1, (0, 0, 0))
+    # Escreve o tempo de partida:
+    text = f.render("Tempo: " + str(tempo_partida) + "s", 1, (0, 0, 0))
     screen.blit(text, (20, 180, 30, 210))
 
 
-def create_board():
-    current_index = 0
-    for card in board:
-        x = 300 + CARD_WIDTH * (current_index % 4) + 40 * (current_index % 4)
-        y = 20 + CARD_HEIGHT * (current_index // 4) + 20 * (current_index // 4)
-        rect = pygame.Rect(x, y, CARD_WIDTH, CARD_HEIGHT)
+# Método para criar o tabuleiro:
+def criar_tabuleiro():
+    indice_atual = 0
+    for carta in tabuleiro:
+        # Pega a posição X e Y da carta de acordo com a sua posição no tabuleiro (4 cartas por linha e coluna)
+        x = 300 + WIDTH_CARTA * (indice_atual % 4) + 40 * (indice_atual % 4)
+        y = 20 + HEIGHT_CARTA * (indice_atual // 4) + 20 * (indice_atual // 4)
+        rect = pygame.Rect(x, y, WIDTH_CARTA, HEIGHT_CARTA)
 
-        card.set_rectangle(rect)
-        cards_back = pygame.transform.scale(card_backward_image, (CARD_WIDTH, CARD_HEIGHT))
-        screen.blit(cards_back, card.card_rectangle)
-        current_index += 1
+        # Salva a posição da carta
+        carta.set_retangulo(rect)
+
+        # Desenha uma carta virada de costas da posição da carta
+        cards_back = pygame.transform.scale(imagem_costas_carta, (WIDTH_CARTA, HEIGHT_CARTA))
+        screen.blit(cards_back, carta.retangulo_carta)
+        indice_atual += 1
 
 
+# Método para tratar os cliques do usuário
 def click_handler(mouse_x, mouse_y):
-    if leave_button.collidepoint(mouse_x, mouse_y):
-        sys.exit()
-    elif restart_button.collidepoint(mouse_x, mouse_y):
-        start_match()
-    else:
-        for card in board:
-            if card.card_rectangle.collidepoint(mouse_x, mouse_y) and card.is_backward:
-                card.flip_card()
+    if botao_sair.collidepoint(mouse_x, mouse_y):           # Caso o usuário tenha clicado no botão de sair da partida...
+        sys.exit()                                          # Fecha o jogo
+    elif botao_reiniciar.collidepoint(mouse_x, mouse_y):    # Caso o usuário tenha clicado no botão de reiniciar...
+        iniciar_partida()                                   # Chama o método para iniciar uma nova partida
+    else:  # O usuário clicou sobre uma carta:
+        for carta in tabuleiro:                             # Verifica sobre qual carta o usuário clicou             
+            if carta.retangulo_carta.collidepoint(mouse_x, mouse_y) and carta.de_costas: # Encontrou a carta, e ela está de costas
+                # Vira a carta
+                carta.virar_carta()
 
+                # Para qualquer som que esteja ocorrendo e toca o som de virada de carta
                 pygame.mixer.stop()
-                flip_sound.play()
+                som_virada.play()
 
-                pygame.draw.rect(screen, (255, 255, 255), card.card_rectangle)
-                c = pygame.transform.scale(card.card_image, (CARD_WIDTH, CARD_HEIGHT))
-                screen.blit(c, card.card_rectangle)
+                # Desenha na tela a carta de frente, em sua posição
+                pygame.draw.rect(screen, (255, 255, 255), carta.retangulo_carta)
+                c = pygame.transform.scale(carta.imagem_carta, (WIDTH_CARTA, HEIGHT_CARTA))
+                screen.blit(c, carta.retangulo_carta)
 
-                current_pair.append(card)
-                global is_wrong, score
-                if len(current_pair) == 2:
-                    if current_pair[0].card_name != current_pair[1].card_name:
-                        global last_wrong_time
-                        last_wrong_time = pygame.time.get_ticks()
-                        is_wrong = True
-                        score -= 1  
-                    else: 
+                # Verifica se é a primeira ou a segunda carta do par atual: 
+                par_atual.append(carta)
+                global esta_errado, pontuacao
+                if len(par_atual) == 2: # Indica que é a segunda carta do par, e assim o par já está formado
+                    if par_atual[0].nome_carta != par_atual[1].nome_carta:    # O par está errado:
+                        global tempo_ultimo_erro
+                        # Avisa ao programa que, durante o loop, deverá tratar o viramento das cartas
+                        tempo_ultimo_erro = pygame.time.get_ticks()
+                        esta_errado = True
+                        # Diminui a posição do usuário por errar
+                        pontuacao -= 1  
+                    else:   # O usuário acertou o par:
+                        # Toca som de acerto
                         pygame.mixer.stop()
-                        score_sound.play()
-                        global how_many_pairs
-                        is_wrong = False
-                        score += 3
-                        how_many_pairs += 1
-                        current_pair.clear()
+                        som_pontuacao.play()
+                        global qtos_pares
+                        esta_errado = False
+                        # Adiciona a pontuação ao usuário
+                        pontuacao += 3
+                        # Adiciona mais um par ao usuário
+                        qtos_pares += 1
+                        par_atual.clear()
                 break
 
-def wrong_pair():
+# Método que será chamado quando o usuário errar um par:
+def par_errado():
+    # Toca o som de erro:
     pygame.mixer.stop()
-    error_sound.play()
+    som_erro.play()
 
-    current_pair[0].flip_card()
-    current_pair[1].flip_card()
+    # Pega as duas cartas do par atual e as vira de costas novamente
+    par_atual[0].virar_carta()
+    par_atual[1].virar_carta()
 
-    pygame.draw.rect(screen, (255, 255, 255), current_pair[0].card_rectangle)
-    card1 = pygame.transform.scale(card_backward_image, (CARD_WIDTH, CARD_HEIGHT))
-    screen.blit(card1, current_pair[0].card_rectangle)
+    # Desenha na tela as cartas viradas de costas
+    # CARTA 1:
+    pygame.draw.rect(screen, (255, 255, 255), par_atual[0].retangulo_carta)
+    card1 = pygame.transform.scale(imagem_costas_carta, (WIDTH_CARTA, HEIGHT_CARTA))
+    screen.blit(card1, par_atual[0].retangulo_carta)
 
-    pygame.draw.rect(screen, (255, 255, 255), current_pair[1].card_rectangle)
-    card1 = pygame.transform.scale(card_backward_image, (CARD_WIDTH, CARD_HEIGHT))
-    screen.blit(card1, current_pair[1].card_rectangle)
+    # CARTA 2:
+    pygame.draw.rect(screen, (255, 255, 255), par_atual[1].retangulo_carta)
+    card1 = pygame.transform.scale(imagem_costas_carta, (WIDTH_CARTA, HEIGHT_CARTA))
+    screen.blit(card1, par_atual[1].retangulo_carta)
 
-    current_pair.clear()
-    global is_wrong, last_wrong_time
-    is_wrong = False
-    last_wrong_time = 0
+    # Limpa a lista que contém as cartas do par atual
+    par_atual.clear()
+    global esta_errado, tempo_ultimo_erro
+    # Prepara o programa para um eventual futuro erro do usuário
+    esta_errado = False
+    tempo_ultimo_erro = 0
 
-
-def check_win():
-    if how_many_pairs == 8:
+# Método para verificar caso o jogador já acertou todas as cartas
+def verifica_vitoria():
+    # Verifica se a quantidade de pares acertados pelo usuário é igual à quantidade máxima de pares
+    if qtos_pares == 8:
+        # Toca som de vitória
         pygame.mixer.stop()
-        victory_sound.play()
-        global match_is_running
-        match_is_running = False
-        ctypes.windll.user32.MessageBoxW(0, "Parabéns! Você venceu o jogo fazendo " + str(score) + " pontos!", "Fim de Jogo!", 1)
+        som_vitoria.play()
+        global partida_acontecendo
+        partida_acontecendo = False
+        # Exibe a mensagem de erro
+        ctypes.windll.user32.MessageBoxW(0, "Parabéns! Você venceu o jogo fazendo " + str(pontuacao) + " pontos!", "Fim de Jogo!", 1)
 
 
-clock = pygame.time.Clock()
+relogio = pygame.time.Clock() # Inicia o relógio
 def run():
     while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN and not is_wrong:
-                clicked_x, clicked_y = pygame.mouse.get_pos()
-                click_handler(clicked_x, clicked_y)
+            elif evento.type == pygame.MOUSEBUTTONDOWN and not esta_errado:     # O usuário clicou em alguma posição
+                x_clicado, y_clicado = pygame.mouse.get_pos()               
+                click_handler(x_clicado, y_clicado)                             # Chama o método para tratar o clique
             
-        time_now = pygame.time.get_ticks()
-        draw_menu()
+        # Pega o tempo atual do programa
+        tempo_atual = pygame.time.get_ticks()
+        # Redesenha o menu a cada frame
+        desenhar_menu()
 
-        if is_wrong and time_now - last_wrong_time >= 1000:
-            wrong_pair()
+        # O usuário errou um par e já foi dado um segundo para que ele visualizasse as cartas:
+        if esta_errado and tempo_atual - tempo_ultimo_erro >= 1000:
+            par_errado()
 
-        if match_is_running:
-            global match_time
-            match_time = (time_now - match_start_time) // 1000
-            draw_menu()
+        # Caso a partida esteja em andamento, atualiza informações do menu
+        if partida_acontecendo:
+            global tempo_partida
+            tempo_partida = (tempo_atual - tempo_inicio_partida) // 1000    # Atualiza o tempo do andamento da partida
+            desenhar_menu()
 
+        # Atualiza a tela
         pygame.display.flip()
 
-        if match_is_running:
-            check_win()
+        # Verifica se o usuário acertou todos os pares
+        if partida_acontecendo:
+            verifica_vitoria()
 
 
-start_match()
+iniciar_partida()
 run()
